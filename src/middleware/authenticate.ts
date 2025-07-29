8fc11f7a0230484f8ccf0a6d6dc0510d599d4451
mattRightCT <matt@rightct.co>
2025-07-29 17:51:22 -0400

    auth

---
+++ src/middleware/authenticate.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+
+import config from "../../config/index.js"
+
+export const authHeaderProperty = 'authorization'
+
+function authenticate(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+): void {
+  const authString = req.headers[authHeaderProperty]
+  if (authString != config.secret) {
+    res.status(401).send('You can only proceed if you know my secret')
+    return
+  }
+  next()
+}
+
+export default authenticate
