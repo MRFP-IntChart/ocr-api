93576a19d93b7fd3986edba569c3eaafd212b3a2
mattRightCT <matt@rightct.co>
2025-07-29 17:52:51 -0400

    only one route

---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import {Route, routerFactory} from './types'
+import controller from '../controller/index.js'
+
+const routes: Route[] = [
+  {
+    method: 'post',
+    route: '/pdf_to_text',
+    controllerMethod: controller.pdfToText
+  }
+]
+
+export default routerFactory(routes)
