4f424d0682867905b1d2360b399881ba6cb710a8
mattRightCT <matt@rightct.co>
2025-07-30 16:01:12 -0400

    additional auth tests

--- tests/controller.spec.ts
+++ tests/controller.spec.ts
@@ -3,6 +3,8 @@ import chaiHttp, {request} from 'chai-http'
 import 'mocha'
 
 import api from '../src/api.js'
+import {authHeaderProperty} from '../src/middleware/authenticate.js'
+import config from '../config/index.js'
 
 chai.use(chaiHttp)
 
@@ -14,6 +16,20 @@ describe('controller', () => {
       it('should error without auth field', async () => {
         const res = await request.execute(api).post('/pdf_to_text').send({})
         expect(res.status).to.equal(401)
+      })
+
+      it('should error if auth string is not correct', async () => {
+        const res = await request.execute(api).post('/pdf_to_text')
+          .set(authHeaderProperty, 'randomstringofletters')
+          .send({})
+        expect(res.status).to.equal(401)
+      })
+
+      it('should succeed if auth string is correct', async () => {
+        const res = await request.execute(api).post('/pdf_to_text')
+          .set(authHeaderProperty, config.secret)
+          .send({})
+        expect(res.status).to.equal(200)
       })
     })
   })
