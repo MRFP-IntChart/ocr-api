5f5977a0e889b53146b0ebe2f086125b657ba71e
mattRightCT <matt@rightct.co>
2025-07-31 12:24:20 -0400

    endpoint

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,10 +1,19 @@
 import express from 'express'
+import fs from 'node:fs/promises'
+// No TS types, so declared in /src/types/global.d.ts
+import scribe from 'scribe.js-ocr'
 
 import {addErrorHandlingToController} from '../error/index.js'
+import {fileProcessing} from '../file/index.js'
 
 
 async function pdfToText(req: express.Request, res: express.Response) {
-  res.send('This is one way to add a body')
+  const file = await fileProcessing(req, res)
+  const filePath = file.path
+  const fileText = await scribe.extractText([filePath])
+  // clean up new file
+  await fs.unlink(filePath)
+  res.send({text: fileText})
 }
 
 const methods = {
